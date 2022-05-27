6cb6ebcdd47b8d1fa0d890f3dde1141e59392a84
이의정 <onairsparks@gmail.com>
2022-05-27 11:22:56 +0900

    logicCode 구현

--- src/test/java/io/herdle/modeling/herdle/HerdleApplicationTests.java
+++ src/test/java/io/herdle/modeling/herdle/HerdleApplicationTests.java
@@ -1,8 +1,11 @@
 package io.herdle.modeling.herdle;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import io.herdle.modeling.herdle.Entity.PollOption;
-import io.herdle.modeling.herdle.Repository.PollOptionRepository;
+import io.herdle.modeling.herdle.Dto.PollSummary;
+import io.herdle.modeling.herdle.Dto.PollSummaryResponse;
+import io.herdle.modeling.herdle.Dto.PollVoteRequest;
+import io.herdle.modeling.herdle.Entity.*;
 import io.herdle.modeling.herdle.Repository.PollRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,84 +14,147 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @ExtendWith(MockitoExtension.class)
 @Transactional
 @SpringBootTest
 class HerdleApplicationTests {
 
-	@InjectMocks
-	PollRepository pollRepository;
-
-	@Autowired
-	PollOptionRepository pollOptionRepository;
-
-	@Mock
-	PollOption pollOption;
+	@PersistenceContext
+	EntityManager em;
 
 
 	@Test
 	void contextLoads() {
-//		pollOption.setLabel("c");
-//		pollOption.setPosition(1);
-//		PollOption savePollOption = pollOptionRepository.save(pollOption);
-//		System.out.println("savePollOption : " + savePollOption.getLabel());
-//		if (savePollOption.getId() != null){
-//			PollOption findPollOption = pollOptionRepository.findById(savePollOption.getId()).orElse(null);
-//			System.out.println("findPollOption : " + findPollOption.toString());
-//		}else{
-//			System.out.println("savePollOption.getId() is null");
-//		}
-//
-//		System.out.println("PollOptions : " + pollOptionRepository.findAll().toString());
 	}
 
 	@Test
-	@Rollback(value = false)
 	@Transactional
-	public void saveData(){
+	public void saveBaseData(){
+
+		JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+
 		System.out.println("save start");
 
-		PollOption pollOption = new PollOption();
-		pollOption.setLabel("C");
-		pollOption.setPosition(1);
-		pollOptionRepository.save(pollOption);
+		PollOption pollOptionC = new PollOption();
+		pollOptionC.setLabel("c");
+		pollOptionC.setPosition(1);
+		em.persist(pollOptionC);
 
-//		pollRepository.savePollOption("c",1);
-//		System.out.println("1 pollOptions : " + pollRepository.selectPollOptions().toString());
-//		pollRepository.savePollOption("c+",2);
-//		System.out.println("2 pollOptions : " + pollRepository.selectPollOptions().toString());
-//		pollRepository.savePollOption("java",3);
-//		System.out.println("3 pollOptions : " + pollRepository.selectPollOptions().toString());
-//		pollRepository.savePollOption("vue.js",4);
-//		System.out.println("4 pollOptions : " + pollRepository.selectPollOptions().toString());
-//		pollRepository.savePollOption("react",5);
-//		System.out.println("5 pollOptions : " + pollRepository.selectPollOptions().toString());
-//
-//
-//		pollRepository.savePollAnswer("c");
-//		pollRepository.savePollAnswer("c");
-//		pollRepository.savePollAnswer("java");
-//		pollRepository.savePollAnswer("vue.js");
-//		pollRepository.savePollAnswer("c+");
-//		pollRepository.savePollAnswer("java");
+		PollOption pollOptionCPlus = new PollOption();
+		pollOptionCPlus.setLabel("c+");
+		pollOptionCPlus.setPosition(2);
+		em.persist(pollOptionCPlus);
+
+		PollOption pollOptionJava = new PollOption();
+		pollOptionJava.setLabel("java");
+		pollOptionJava.setPosition(3);
+		em.persist(pollOptionJava);
+
+		PollOption pollOptionReact = new PollOption();
+		pollOptionReact.setLabel("react");
+		pollOptionReact.setPosition(4);
+		em.persist(pollOptionReact);
+
+		List<PollOption> pollOptions = new ArrayList<>();
+		pollOptions.add(pollOptionC);
+		pollOptions.add(pollOptionCPlus);
+		pollOptions.add(pollOptionJava);
+		pollOptions.add(pollOptionReact);
+
+		Poll poll = new Poll();
+		poll.setPollOptions(pollOptions);
+		em.persist(poll);
+
+
+
+		Random random = new Random();
+		for (int i = 0; i < 100; i++) {
+
+			PollAnswer pollAnswer = new PollAnswer();
+
+			int k = random.nextInt(4) + 1;
+
+
+			QPollOption qPollOption = QPollOption.pollOption;
+			List<PollOption> pollOptionList = jpaQueryFactory
+					.selectFrom(qPollOption)
+					.fetch();
+
+			for (PollOption pollOption : pollOptionList) {
+				if (pollOption.getPosition() == (long)k){
+					pollAnswer.setUserChoice(pollOption.getLabel());
+					em.persist(pollAnswer);
+				}
+			}
+		}
+
 		System.out.println("save end");
 	}
 
 	@Test
 	public void selectPollOptions(){
-		saveData();
-		System.out.println("pollOptions : " + pollRepository.selectPollOptions().toString());
+		saveBaseData();
+		JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+
+		QPoll qPoll = QPoll.poll;
+
+		Poll poll = jpaQueryFactory
+				.selectFrom(qPoll)
+				.fetchOne();
+
+		QPollAnswer qPollAnswer = QPollAnswer.pollAnswer;
+
+		List<PollAnswer> pollAnswers = jpaQueryFactory
+				.selectFrom(qPollAnswer)
+				.fetch();
+
+		System.out.println("poll : " + poll.toString());
+		System.out.println("pollOptions : " + poll.getPollOptions().toString());
+		System.out.println("pollAnswers : " + pollAnswers.toString());
 	}
 
 	@Test
 	public void logicCode(){
-		saveData();
-		System.out.println("PollSummaryResponse : " + pollRepository.logicCode().toString());
+
+		saveBaseData();
+
+		QPoll qPoll = QPoll.poll;
+		QPollOption qPollOption = QPollOption.pollOption;
+		QPollAnswer qPollAnswer = QPollAnswer.pollAnswer;
+
+		JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+
+		Poll poll = jpaQueryFactory
+				.selectFrom(qPoll)
+				.fetchOne();
+
+		PollVoteRequest pollVoteRequest = new PollVoteRequest();
+		pollVoteRequest.setId(poll.getId());
+
+		//logicCode start
+		List<PollSummary> response = jpaQueryFactory.select(Projections.constructor(PollSummary.class, qPollOption.label, qPollAnswer.userChoice.count().as("votes")))
+				.from(qPoll)
+				.join(qPoll.PollOptions, qPollOption)
+				.leftJoin(qPollAnswer).on(qPollOption.label.eq(qPollAnswer.userChoice))
+				.where(qPoll.id.eq(pollVoteRequest.getId()))
+				.groupBy(qPollOption.label)
+				.orderBy(qPollOption.position.asc())
+				.fetch();
+
+		PollSummaryResponse summaryResponse = new PollSummaryResponse();
+		summaryResponse.setSummaries(response);
+		summaryResponse.setNumberOfVoters(response.stream().mapToLong(r -> r.getVotes()).sum());
+		//logicCode end
+
+		System.out.println("summaryResponse : " + summaryResponse.toString());
 	}
 
 }
